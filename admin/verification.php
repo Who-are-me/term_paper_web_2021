12bae3fd8aefaeb2bf307e4ecbe1002aa3279174
Forever <vasyastorchak74@gmail.com>
2021-04-18 13:30:33 +0300

    add verifacation password with db

--- admin/verification.php
+++ admin/verification.php
@@ -1,2 +1,49 @@
 <?php
-echo "hello";
+
+$connection = mysqli_connect('localhost', 'root', '', 'db_course_work');
+
+if ($connection === false) {
+    echo mysqli_connect_error();
+    exit();
+}
+
+$query = "select password from accounts where login = '" . $_POST['login']. "'";
+
+$result = mysqli_query($connection, $query);
+
+$cat = mysqli_fetch_assoc($result);
+
+if(empty($cat)) {
+    echo file_get_contents("index.html");
+    exit();
+}
+
+
+if(md5($_POST['password']) === $cat['password']) {
+    echo "yes";
+}
+else {
+    echo "no";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+mysqli_close($connection);
