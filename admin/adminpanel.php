4d451a861f5f02f685b93dc71d4d8ee78a7a7f74
Forever <vasyastorchak74@gmail.com>
2021-04-19 11:05:28 +0300

    HELP

--- admin/adminpanel.php
+++ admin/adminpanel.php
@@ -14,7 +14,7 @@ if(empty($_SERVER['HTTP_REFERER']) || ($_SESSION["verification"] != true)) {
     <meta charset="utf-8">
     <title>Admin panel</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="../style/main.css">
+    <link rel="stylesheet" type="text/css" href="../style/adminpanel.css">
   </head>
 
   <body class="main">
