20c4b2f9707f6ea5590d0df365a480b5364a99fa
lukovskyj <volodymyr.lukovskyj@gmail.com>
2021-06-05 18:35:18 +0300

    aganin one more commit

---
+++ templates/files for testing/slider-slid.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.slider').slick({
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots: true
+  
+    });
+        var width = $('img', '.slider').width();
+          $('img', '.slider').height(width);
+  });
+
+  function res(){
+    var imgAutoSize = $('img', '.slider').width();
+      $('img', '.slider').height(imgAutoSize);
+ } res();
+ 
+ $( wiimgAutoSizendow ).resize(function() {
+     res();
+ });
