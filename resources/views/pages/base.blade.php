fddab643ba8349f629776319aa265c52322b8131
Forever <vasyastorchak74@gmail.com>
2021-06-14 21:57:14 +0300

    t

--- resources/views/pages/base.blade.php
+++ resources/views/pages/base.blade.php
@@ -5,15 +5,13 @@
 @section('content')
 <link href="css/pages.css" rel="stylesheet">
 
-    <div class="row d-flex m-0 p-0 title-material">
-        <div class="col-md-1 col-lg-2 d-none d-md-flex p-0 m-0"></div>
-        <div class="col-12 col-md-10 col-lg-8 text-center py-3 py-md-5 px-5 px-md-0">
-            <div class="col-12 py-1 py-md-4 px-md-4  material-base-title-inside-cont text-center" data-aos="zoom-out" data-aos-duration="1000">
-                <span>Матеріальна база</span>
-            </div>
+<div class="row d-flex m-0 p-0 title-material">
+    <div class="col-md-1 col-lg-2 d-none d-md-flex p-0 m-0"></div>
+    <div class="col-12 col-md-10 col-lg-8 text-center py-3 py-md-5 px-5 px-md-0">
+        <div class="col-12 py-1 py-md-4 px-md-4  material-base-title-inside-cont text-center" data-aos="zoom-out" data-aos-duration="1000">
+            <span>Матеріальна база</span>
         </div>
     </div>
-    
 </div>
 
 <div class="row d-flex m-0 p-0 content-background-material px-5 px-md-0">
@@ -24,10 +22,11 @@
             <div class="col-12 p-0 d-flex">
                 <div class="col-1 d-none d-sm-flex p-0"></div>
                 <div class="col-12 col-sm-10 d-flex p-0">
-                   <p class="text-center basic-text p-0">у повній мірі забезпечений необхідними 
-                    <span class="basic-text-bold">навчальними приміщеннями,</span>  обладнаними необхідними 
-                    <span class="basic-text-bold">приладами</span>  та 
-                    <span class="basic-text-bold">пристроями</span>  для опанування тем навчальної програми. </p> 
+                    <p class="text-center basic-text p-0">у повній мірі забезпечений необхідними 
+                        <span class="basic-text-bold">навчальними приміщеннями,</span>  обладнаними необхідними 
+                        <span class="basic-text-bold">приладами</span>  та 
+                        <span class="basic-text-bold">пристроями</span>  для опанування тем навчальної програми. 
+                    </p> 
                 </div>
                 <div class="col-1 d-none d-sm-flex p-0"></div>
             </div>
@@ -42,55 +41,90 @@
         <div class="col-12 p-0 m-0">
             <div class="col-12 d-block d-md-flex p-0  m-0 py-3 py-md-5">
                 <div class="col-12 col-md-6 p-0 text-center text-md-start" data-aos="fade-right" data-aos-duration="1500">
-                    <img src="#" class="pr-md-5 w-100">Тут має бути картинка
+                    <img src="/files/fea_logo.jpg" class="pr-md-5 w-100">
                 </div> 
                 <div class="col-12 col-md-6 text-center text-md-left  align-items-center pl-md-3" data-aos="fade-left" data-aos-duration="1500">
                     <p class="basic-text-bold">Кабінет-лабораторія телекомунікаційних пристроїв приймання та передавання інформації, телевізійної та офісної техніки (ауд.321).</p>
                 </div>
             </div>
+
             <div class="col-12 d-block d-md-flex p-0  m-0 py-3 py-md-5">
                 <div class="col-12 col-md-6 order-md-last p-0 text-center text-md-start " data-aos="fade-left" data-aos-duration="1500">
-                    <img src="#" class="w-100 pl-md-5">Тут має бути картинка
+                    <img src="/files/fea_logo.jpg" class="w-100 pl-md-5">
                 </div>
                 <div class="col-12 col-md-6 align-middle text-center text-md-right order-md-first p-0 pr-md-3" data-aos="fade-right" data-aos-duration="1500">
                     <p class="basic-text-bold">Кабінет-лабораторія конструювання та технології виробництва РЕА, систем автоматизованого проектування (ауд. 306).</p>
 
                 </div>
             </div>
-            <div class="col-12 d-block d-md-flex p-0  m-0 py-3 py-md-5">
-                <div class="col-12 col-md-6 p-0 text-center text-md-start" data-aos="fade-right" data-aos-duration="1500">
-                    <img src="#" class="pr-md-5 w-100">Тут має бути картинка
-                </div>
-                <div class="col-12 col-md-6 text-center text-md-left  align-items-center pl-md-3" data-aos="fade-left" data-aos-duration="1500">
-                    <p class="basic-text-bold">Кабінет-лабораторія радіопередавальних пристроїв, метрології та вимірювальної техніки (ауд.310) .</p>
-                </div>
-            </div>
-            <div class="col-12 d-block d-md-flex p-0  m-0 py-3 py-md-5">
-                <div class="col-12 col-md-6 order-md-last p-0 text-center text-md-start " data-aos="fade-left" data-aos-duration="1500">
-                    <img src="#" class="w-100 pl-md-5">Тут має бути картинка
-                </div>
 
-                <div class="col-12 col-md-6 align-middle text-center text-md-right order-md-first p-0 pr-md-3" data-aos="fade-right" data-aos-duration="1500">
-                    <p class="basic-text-bold">Кабінет-лабораторія радіопередавальних пристроїв, метрології та вимірювальної техніки (ауд.310) .</p>
-                </div>
-            </div>
             <div class="col-12 d-block d-md-flex p-0  m-0 py-3 py-md-5">
                 <div class="col-12 col-md-6 p-0 text-center text-md-start" data-aos="fade-right" data-aos-duration="1500">
-                    <img src="#" class="pr-md-5 w-100">Тут має бути картинка
+                    <img src="/files/fea_logo.jpg" class="pr-md-5 w-100">
                 </div>
                 <div class="col-12 col-md-6 text-center text-md-left  align-items-center pl-md-3" data-aos="fade-left" data-aos-duration="1500">
                     <p class="basic-text-bold">Кабінет-лабораторія цифрових пристроїв, сигналів та процесів в радіотехніці, електронних пристроїв та мікроелектроніки (ауд.313).</p>
+                </div>
+            </div>
+
+            <div class="col-12 d-block d-md-flex p-0  m-0 py-3 py-md-5">
+                <div class="col-12 col-md-6 order-md-last p-0 text-center text-md-start " data-aos="fade-left" data-aos-duration="1500">
+                    <img src="/files/fea_logo.jpg" class="w-100 pl-md-5">
+                </div>
+
+                <div class="col-12 col-md-6 align-middle text-center text-md-right order-md-first p-0 pr-md-3" data-aos="fade-right" data-aos-duration="1500">
+                    <p class="basic-text-bold">Лабораторія пристроїв НВЧ та антен (ауд. 314).</p>
+                </div>
+            </div>
+
+            <div class="col-12 d-block d-md-flex p-0  m-0 py-3 py-md-5">
+                <div class="col-12 col-md-6 p-0 text-center text-md-start" data-aos="fade-right" data-aos-duration="1500">
+                    <img src="/files/fea_logo.jpg" class="pr-md-5 w-100">
+                </div>
+                <div class="col-12 col-md-6 text-center text-md-left  align-items-center pl-md-3" data-aos="fade-left" data-aos-duration="1500">
+                    <p class="basic-text-bold">Кабінет-лабораторія радіопередавальних пристроїв, метрології та вимірювальної техніки (ауд.310).</p>
 
                 </div>
             </div>
+
             <div class="col-12 d-block d-md-flex p-0  m-0 py-3 py-md-5">
                 <div class="col-12 col-md-6 order-md-last p-0 text-center text-md-start " data-aos="fade-left" data-aos-duration="1500">
-                    <img src="#" class="w-100 pl-md-5">Тут має бути картинка
+                    <img src="/files/fea_logo.jpg" class="w-100 pl-md-5">
                 </div>
                 <div class="col-12 col-md-6 align-middle text-center text-md-right order-md-first p-0 pr-md-3" data-aos="fade-right" data-aos-duration="1500">
-                    <p class="basic-text-bold">Кабінет-лабораторія радіопередавальних пристроїв, метрології та вимірювальної техніки (ауд.310) .</p>
+                    <p class="basic-text-bold">Кабінет-лабораторія схемотехнічного проектування радіотехнічних пристроїв та радіотехнічних систем (ауд. 213).</p>
                 </div>
             </div>
+
+            <div class="col-12 d-block d-md-flex p-0  m-0 py-3 py-md-5">
+                <div class="col-12 col-md-6 p-0 text-center text-md-start" data-aos="fade-right" data-aos-duration="1500">
+                    <img src="/files/fea_logo.jpg" class="pr-md-5 w-100">
+                </div>
+                <div class="col-12 col-md-6 text-center text-md-left  align-items-center pl-md-3" data-aos="fade-left" data-aos-duration="1500">
+                    <p class="basic-text-bold">Кабінет-лабораторія теорії кіл, електроматеріалознавства та елементної бази РЕА (ауд.217).</p>
+                </div>
+            </div>
+
+            <div class="col-12 d-block d-md-flex p-0  m-0 py-3 py-md-5">
+                <div class="col-12 col-md-6 order-md-last p-0 text-center text-md-start " data-aos="fade-left" data-aos-duration="1500">
+                    <img src="/files/fea_logo.jpg" class="w-100 pl-md-5">
+                </div>
+
+                <div class="col-12 col-md-6 align-middle text-center text-md-right order-md-first p-0 pr-md-3" data-aos="fade-right" data-aos-duration="1500">
+                    <p class="basic-text-bold">Кабінет-лабораторія автоматики та схемотехніки мікропроцесорних систем (ауд.219).</p>
+                </div>
+            </div>
+
+            <div class="col-12 d-block d-md-flex p-0  m-0 py-3 py-md-5">
+                <div class="col-12 col-md-6 p-0 text-center text-md-start" data-aos="fade-right" data-aos-duration="1500">
+                    <img src="/files/fea_logo.jpg" class="pr-md-5 w-100">
+                </div>
+                <div class="col-12 col-md-6 text-center text-md-left  align-items-center pl-md-3" data-aos="fade-left" data-aos-duration="1500">
+                    <p class="basic-text-bold">Лабораторія джерел електроживлення та стандартизації РЕА (ауд. 223).</p>
+
+                </div>
+            </div>
+
         </div>
         <div class="col-12 p-0 text-center d-flex">
             <div class="col-1"></div>
