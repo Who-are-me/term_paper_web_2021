225946a53b671a8058ac6183008040fd471bc2b5
FUL1ER <49909424+FUL1ER@users.noreply.github.com>
2021-06-14 20:21:07 +0300

    for Wowa

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -16,20 +16,29 @@
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img class="d-block w-100" src="{{ $content[0]->text }}" alt="First slide" style="object-fit: cover; height: 550px;">
-      <div class="carousel-caption d-block title-slide">
-        <span class="title-slide-text">ЦК Радіотехнічних Дисциплін</span>
+      <div class="carousel-caption d-block ">
+        <div class="title-slide">
+          <span class="title-slide-text">ЦК Радіотехнічних Дисциплін</span>
+          <p class="sub-title-slide-text">Свіжа та докладна інформація зі сфери раідотехнологій</p>
+        </div>
       </div>
     </div>
     <div class="carousel-item">
       <img class="d-block w-100" src="{{ $content[1]->text }}" alt="Second slide" style="object-fit: cover; height: 550px;">
-      <div class="carousel-caption d-block title-slide">
-      <span class="title-slide-text">ЦК Радіотехнічних Дисциплін</span>
+      <div class="carousel-caption d-block">
+        <div class="title-slide">
+          <span class="title-slide-text">ЦК Радіотехнічних Дисциплін</span>
+          <p class="sub-title-slide-text">Свіжа та докладна інформація зі сфери раідотехнологій</p>
+        </div>
       </div>
     </div>
     <div class="carousel-item">
       <img class="d-block w-100" src="{{ $content[2]->text }}" alt="Third slide" style="object-fit: cover; height: 550px;">
-      <div class="carousel-caption d-block title-slide">
-      <span class="title-slide-text">ЦК Радіотехнічних Дисциплін</span>
+      <div class="carousel-caption d-block">
+        <div class="title-slide">
+          <span class="title-slide-text">ЦК Радіотехнічних Дисциплін</span>
+          <p class="sub-title-slide-text">Свіжа та докладна інформація зі сфери раідотехнологій</p>
+        </div>
       </div>
     </div>
   </div>
